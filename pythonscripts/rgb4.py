015d4ab8c93384083b70ac5ad2cbacc3a60af77c
gaterkik <kik.gater@gmail.com>
2020-08-16 21:41:30 +0200

    change DD bedroom

--- pythonscripts/rgb4.py
+++ pythonscripts/rgb4.py
@@ -1,8 +1,22 @@
 import http.client
 from time import sleep
 import random
+
+# retrieve String item
+# r = requests.get('http://192.168.0.6:8080/rest/items/itemPHP4/state')
+# data = r.content
+# print(data)
+
+r = http.client.HTTPConnection("192.168.0.6:8080")
+strrequest = "/rest/items/itemPHP4/state"
+r.request("GET", strrequest)
+response = r.getresponse()
+print(type(response.read()))
+print(response.read().decode("utf-8"))
+
+
 array = ["61bb46", "fdb827", "f5821f", "e03a3e", "963d97", "009ddc"]
-while True:
+while response.read() == "b'ON'":
     cmd1 = ''
     for i in range(0,58):
         cmd1 += random.choice(array) 
