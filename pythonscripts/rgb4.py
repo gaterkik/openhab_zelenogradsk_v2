c43b15da0603704aa0d26ee5a8196f3f129e8fa9
gaterkik <kik.gater@gmail.com>
2020-09-20 09:25:36 +0200

    fix i2c sensor

--- pythonscripts/rgb4.py
+++ pythonscripts/rgb4.py
@@ -1,39 +1,13 @@
 import http.client
 from time import sleep
 import random
-import requests
-
-# retrieve String item
-r = requests.get('http://192.168.0.6:8080/rest/items/itemPHP4/state')
-data = r.content
-print(data.decode("utf-8"))
-
-# requests.put('http://192.168.1.100:8080/rest/items/Spotlight/state', 'ON')
-
-# g = requests.get('http://192.168.0.6:8080/rest/items/itemPHP4/state')
-# data = g.content
-# print('itemPHP4 = '+data)
-
-
-
-# r = http.client.HTTPConnection("192.168.0.6:8080")
-# strrequest = "/rest/items/itemPHP4/state"
-# r.request("GET", strrequest)
-# response = r.getresponse()
-# print(type(response.read()))
-# print(response.read())
-# print(response.read().decode("utf-8"))
-
 array = ["61bb46", "fdb827", "f5821f", "e03a3e", "963d97", "009ddc"]
 while True:
-    r = requests.get('http://192.168.0.6:8080/rest/items/itemPHP4/state')
-    data = r.content
-    if data.decode("utf-8") == 'ON': 
-        cmd1 = ''
-        for i in range(0,58):
-            cmd1 += random.choice(array) 
-            
-        conn = http.client.HTTPConnection("192.168.0.15")
-        strrequest = "/sec/?pt=35&ws={}".format(cmd1)
-        conn.request("GET", strrequest)
-        sleep(1)
+    cmd1 = ''
+    for i in range(0,58):
+        cmd1 += random.choice(array) 
+        
+    conn = http.client.HTTPConnection("192.168.0.15")
+    strrequest = "/sec/?pt=35&ws={}".format(cmd1,)
+    conn.request("GET", strrequest)
+    sleep(1)
